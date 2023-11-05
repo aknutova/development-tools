915e3191b487565c7723df662a5ffb1f56607df9
aknutova <it31.ak@gmail.com>
2023-11-05 22:34:38 +0200

    Initial Code

---
+++ src/telran/numbers/Calculations.java
@@ -0,0 +1,16 @@
+package telran.numbers;
+
+public class Calculations {
+	public static double sum(double op1, double op2) {
+		return op1 + op2;
+	}
+	public static double subtract(double op1, double op2) {
+		return op1 - op2;
+	}
+	public static double multiply(double op1, double op2) {
+		return op1 * op2;
+	}
+	public static double divide(double op1, double op2) {
+		return op1 / op2;
+	}
+}
